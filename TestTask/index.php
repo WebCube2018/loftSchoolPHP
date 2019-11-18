f9e177e698f3c602fd63e41fc6e4eeee519bf411
Andrei Donets <info@webcube.su>
2019-11-18 19:54:32 +0300

    изменил кодировку после теста, и проверку (задача от Роман Прилипа)

--- TestTask/index.php
+++ TestTask/index.php
@@ -2,7 +2,7 @@
 
 echo "<pre>";
 
-echo "Задание 1".PHP_EOL;
+echo "Р—Р°РґР°РЅРёРµ 1".PHP_EOL;
 
 $var1 = rand(1, 100);
 $var2 = rand(1, 100);
@@ -11,10 +11,10 @@ $sum = $var1+$var2;
 echo $var1." + ".$var2." = ".$sum."<br><br>";
 
 
-echo "Задание 2".PHP_EOL;
+echo "Р—Р°РґР°РЅРёРµ 2".PHP_EOL;
 
 for ($i = 1; $i <= 10; $i++) {
-    if ($i % 2) {
+    if (($i % 2) == 0) {
         echo $i." ";
     }
 }
