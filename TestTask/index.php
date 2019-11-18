70cbacfa1be50d6afe9447d91eed8cfbe1e5151e
Andrei Donets <info@webcube.su>
2019-11-18 19:46:46 +0300

    Сделал тестовую задачу (задача от Роман Прилипа)

---
+++ TestTask/index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<pre>";
+
+echo "Задание 1".PHP_EOL;
+
+$var1 = rand(1, 100);
+$var2 = rand(1, 100);
+$sum = $var1+$var2;
+
+echo $var1." + ".$var2." = ".$sum."<br><br>";
+
+
+echo "Задание 2".PHP_EOL;
+
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2) {
+        echo $i." ";
+    }
+}
