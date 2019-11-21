5139008d7a87942ff1f5ccdb914adeb6f17cde98
Andrei Donets <info@webcube.su>
2019-11-22 00:14:51 +0300

    Сделал домашение задание 1

--- Homework_001/index.php
+++ Homework_001/index.php
@@ -38,4 +38,93 @@ if ($age >= 18 && $age <= 65) {
     echo "Неизвестный возраст";
 }
 
+echo "<br><br><br>";
+
+
+//Задание #4
+$day = rand(1, 10);
+
+switch ($day) {
+    case 1:
+        echo "Это рабочий день";
+        break;
+    case 2:
+        echo "Это рабочий день";
+        break;
+    case 3:
+        echo "Это рабочий день";
+        break;
+    case 4:
+        echo "Это рабочий день";
+        break;
+    case 5:
+        echo "Это рабочий день";
+        break;
+    case 6:
+        echo "Это выходной день";
+        break;
+    case 7:
+        echo "Это выходной день";
+        break;
+    default:
+        echo "Неизвестный день";
+}
+
+echo "<br><br><br>";
+
+
+//Задание #5
+$bmw = [
+    "model" => "X5",
+    "speed" => 120,
+    "doors" => 5,
+    "year" => 2015
+];
+$toyota = [
+    "model" => "RAV4",
+    "speed" => 199,
+    "doors" => 5,
+    "year" => 2018
+];
+$opel = [
+    "model" => "Astra",
+    "speed" => 190,
+    "doors" => 5,
+    "year" => 2017
+];
+
+$car["bmw"] = $bmw;
+$car["toyota"] = $toyota;
+$car["opel"] = $opel;
+
+foreach ($car as $k => $value) {
+    echo "CAR: ".$k."<br>";
+    echo $value["model"]." ".$value["speed"]." ".$value["doors"]." ".$value["year"]."<br>";
+}
+
+echo "<br><br><br>";
+
+
+//Задание #6
+
+//стили для таблицы
+echo "<style> td{height: 35px; width: 35px; border: 1px solid; text-align: center;}</style>";
+
+echo "<table>";
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "<tr>";
+    for ($c = 1; $c <= 10; $c++) {
+        if (($i % 2) == 0 && ($c % 2) == 0) {
+            echo '<td>('.$i*$c.')</td>';
+        } elseif (($i % 2) != 0 && ($c % 2) != 0) {
+            echo '<td>['.$i*$c.']</td>';
+        }
+        echo '<td>'.$i*$c.'</td>';
+    }
+    echo "</tr>";
+}
+
+echo "</table>";
+
 echo "</pre>";
