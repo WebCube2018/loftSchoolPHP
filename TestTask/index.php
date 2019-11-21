324591a5e6539cb0390a55f9edb13ddcc09836ab
Andrei Donets <info@webcube.su>
2019-11-21 12:22:37 +0300

    Отформатировал код согласно стандарту PSR

--- TestTask/index.php
+++ TestTask/index.php
@@ -2,16 +2,18 @@
 
 echo "<pre>";
 
-echo "Задание 1".PHP_EOL;
+echo "Задание 1 <br>";
 
 $var1 = rand(1, 100);
 $var2 = rand(1, 100);
-$sum = $var1+$var2;
+$sum = $var1 + $var2;
 echo $var1." + ".$var2." = ".$sum."<br><br>";
 
-echo "Задание 2".PHP_EOL;
+echo "Задание 2 <br>";
 for ($i = 1; $i <= 10; $i++) {
     if (($i % 2) == 0) {
         echo $i." ";
     }
 }
+
+echo "</pre>";
