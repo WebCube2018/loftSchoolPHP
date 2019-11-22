a471c6987572583e132be6785d82ca450196f160
Andrei Donets <info@webcube.su>
2019-11-22 10:39:03 +0300

    Изменил проверку на switch

--- Homework_001/index.php
+++ Homework_001/index.php
@@ -46,23 +46,13 @@ $day = rand(1, 10);
 
 switch ($day) {
     case 1:
-        echo "Это рабочий день";
-        break;
     case 2:
-        echo "Это рабочий день";
-        break;
     case 3:
-        echo "Это рабочий день";
-        break;
     case 4:
-        echo "Это рабочий день";
-        break;
     case 5:
         echo "Это рабочий день";
         break;
     case 6:
-        echo "Это выходной день";
-        break;
     case 7:
         echo "Это выходной день";
         break;
