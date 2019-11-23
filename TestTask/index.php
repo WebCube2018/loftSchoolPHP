a0427ec132121bd27adfda45cbad154de81ea963
Andrei Donets <info@webcube.su>
2019-11-23 17:08:30 +0300

    привел к стандарту PSR

--- TestTask/index.php
+++ TestTask/index.php
@@ -7,12 +7,12 @@ echo "Задание 1 <br>";
 $var1 = rand(1, 100);
 $var2 = rand(1, 100);
 $sum = $var1 + $var2;
-echo $var1." + ".$var2." = ".$sum."<br><br>";
+echo $var1 . "+" . $var2 . "=" . $sum . "<br><br>";
 
 echo "Задание 2 <br>";
 for ($i = 1; $i <= 10; $i++) {
     if (($i % 2) == 0) {
-        echo $i." ";
+        echo $i . " ";
     }
 }
 
